88022705bdecd9d220b03dd79dee8eb109abb3f3
Dhanush0660 <88339584+Dhanush0660@users.noreply.github.com>
2024-03-28 15:35:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 app.use(function (req, res, next) {
 
-    res.setHeader("Access-Control-Allow-Origin", "https://news-sentiment-analyzer.web.app")
+    res.setHeader("Access-Control-Allow-Origin", "https://frontend-iazu.onrender.com")
     //res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000")
 
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
